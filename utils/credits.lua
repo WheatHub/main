383978247354d8483cec8e993a70c5e8d7485dba
WheatHub <120756906+WheatHub@users.noreply.github.com>
2022-12-23 22:53:50 +0200

    Create credits.lua

---
+++ utils/credits.lua
@@ -0,0 +1,14 @@
+return fucntion(window_ui_rayfield)
+    local Credits_Tab = window_ui_rayfield:CreateTab("Credits")
+
+    Credits_Tab:CreateButton({
+       Name = "Discord Invite: wsbyxxCQuW",
+       Callback = function()
+           if setclipboard then
+              setclipboard("https://discord.gg/wsbyxxCQuW")
+           end
+       end,
+    })
+
+    Credits_Tab:CreateLabel("Owner: MaGiXx#6964")
+end
